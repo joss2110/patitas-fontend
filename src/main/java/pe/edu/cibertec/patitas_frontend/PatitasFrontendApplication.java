c081c2d2ba1c561aaf55fa7c9bf5108d5ebdfcba
joss2110 <g.j.torres.perez@gmail.com>
2024-09-17 21:33:36 -0500

    carga inicial de patitas

---
+++ src/main/java/pe/edu/cibertec/patitas_frontend/PatitasFrontendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.patitas_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatitasFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatitasFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/patitas_frontend/PatitasFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.patitas_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatitasFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
